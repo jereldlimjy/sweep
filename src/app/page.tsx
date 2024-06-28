7384fa4c5cc0b889c179f116dd58ef7aa7a17d42
jereldlimjy <jereldlimjy@hotmail.com>
2024-06-28 18:22:46 +0800

    add kyberswap calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
-import { formatUnits } from "viem";
+import { formatUnits, parseUnits, erc20Abi, encodeFunctionData } from "viem";
 import { formatCurrency } from "@coingecko/cryptoformat";
+import { useSendCalls } from "wagmi/experimental";
 
 const getTokenBalances = async (address: string) => {
   try {
@@ -25,11 +26,12 @@ const getTokenBalances = async (address: string) => {
 };
 
 interface SelectedTokens {
-  [tokenAddress: string]: string;
+  [tokenAddress: string]: any;
 }
 
 function App() {
   const { address } = useAccount();
+  const { sendCalls } = useSendCalls();
   const [tokens, setTokens] = useState<any[]>([]);
   const [selectedTokens, setSelectedTokens] = useState<SelectedTokens>({});
   const [loading, setLoading] = useState(false);
@@ -53,7 +55,8 @@ function App() {
   const handleTokenSelect = (
     tokenAddress: string,
     checked: boolean,
-    value: string
+    value: string,
+    decimals: number
   ) => {
     if (!checked) {
       const currTokens = { ...selectedTokens };
@@ -62,12 +65,93 @@ function App() {
     } else {
       setSelectedTokens((prev) => ({
         ...prev,
-        [tokenAddress]: value,
+        [tokenAddress]: {
+          amount: value,
+          decimals,
+        },
       }));
     }
   };
 
-  const handleSweep = async () => {};
+  const handleSweep = async () => {
+    try {
+      // Get route information
+      const routePromises = Object.keys(selectedTokens).map(
+        async (tokenAddress) => {
+          const queryParam = new URLSearchParams({
+            tokenIn: tokenAddress,
+            tokenOut: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+            amountIn: parseUnits(
+              selectedTokens[tokenAddress].amount,
+              selectedTokens[tokenAddress].decimals
+            ).toString(),
+          });
+          const res = await fetch(
+            `https://aggregator-api.kyberswap.com/base/api/v1/routes?${queryParam.toString()}`
+          );
+          return res.json();
+        }
+      );
+
+      const routes = await Promise.all(routePromises);
+
+      const successfulRoutes = routes.filter((route) => route.code === 0);
+
+      // Get encoded swap route
+      const callsPromises = successfulRoutes.map(async (route) => {
+        const res = await fetch(
+          "https://aggregator-api.kyberswap.com/base/api/v1/route/build",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              routeSummary: route.data.routeSummary,
+              sender: address,
+              recipient: address,
+              slippageTolerance: 10, // 0.1%
+            }),
+          }
+        );
+        return res.json();
+      });
+
+      const callsArray = await Promise.all(callsPromises);
+
+      // Approve calldata
+      const approveCalls = successfulRoutes.map((route) => {
+        const amount = route.data.amountIn;
+
+        // Construct calldata
+        const approveCalldata = encodeFunctionData({
+          abi: erc20Abi,
+          functionName: "approve",
+          args: [address as `0x${string}`, amount],
+        });
+
+        return approveCalldata;
+      });
+
+      console.log(approveCalls);
+
+      sendCalls({
+        calls: callsArray
+          .map((call, idx) => {
+            return [
+              {
+                to: call.data.routerAddress as `0x${string}`,
+                data: approveCalls[idx],
+                value: BigInt(0),
+              },
+              {
+                to: call.data.routerAddress as `0x${string}`,
+                data: call.data.data as `0x${string}`,
+                value: BigInt(0),
+              },
+            ];
+          })
+          .flat(),
+      });
+    } catch (err) {}
+  };
 
   return (
     <div className="flex flex-col w-full items-center justify-center">
@@ -87,7 +171,8 @@ function App() {
                       handleTokenSelect(
                         token.token_address,
                         e.target.checked,
-                        formatUnits(token.balance, token.decimals)
+                        formatUnits(token.balance, token.decimals),
+                        token.decimals
                       )
                     }
                     disabled={parseFloat(token.usdPrice) === 0}
@@ -115,10 +200,11 @@ function App() {
                         handleTokenSelect(
                           token.token_address,
                           true,
-                          e.target.value
+                          e.target.value,
+                          token.decimals
                         )
                       }
-                      value={selectedTokens[token.token_address] ?? "0"}
+                      value={selectedTokens[token.token_address].amount ?? "0"}
                     />
                   )}
                 </div>
